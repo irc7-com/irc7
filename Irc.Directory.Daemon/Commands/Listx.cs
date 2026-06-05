df48e80190fb1c21ceed8f8dc4a7d5a6a8fb3adc
jyonxo <jyonxo@gmail.com>
2026-06-05 10:49:08 +0900

    feat(directory): implement enhanced LISTX command for Directory Server (as per spec) with complex filtering options

--- Irc.Directory.Daemon/Commands/Listx.cs
+++ Irc.Directory.Daemon/Commands/Listx.cs
@@ -3,6 +3,7 @@ using Irc.Constants;
 using Irc.Enumerations;
 using Irc.Helpers;
 using Irc.Interfaces;
+using Irc.Services;
 
 namespace Irc.Directory.Daemon.Commands;
 
@@ -13,6 +14,9 @@ internal class Listx : Command, ICommand
         return EnumCommandDataType.None;
     }
 
+    // Below is custom LISTX command for Directory
+    // It outputs LIST raws but allows for more complex filtering of rooms, as well as categories
+    // LISTX [category code] [query list=[mask]]
     public new void Execute(IChatFrame chatFrame)
     {
         var server = (DirectoryServer)chatFrame.Server;
@@ -26,46 +30,15 @@ internal class Listx : Command, ICommand
 
         var parameters = chatFrame.ChatMessage.Parameters;
         var rooms = server.CacheManager.GetAllRooms().ToList();
-        var firstParam = parameters.FirstOrDefault();
 
-        if (firstParam != null)
+        if (parameters.Count > 0)
         {
-            var queryTerms = Tools.CSVToArray(firstParam);
-            foreach (var term in queryTerms)
-            {
-                if (term == "R=0")
-                {
-                    rooms = rooms.Where(r => !r.Managed).ToList();
-                }
-                else if (term == "R=1")
-                {
-                    rooms = rooms.Where(r => r.Managed).ToList();
-                }
-                else if (term.StartsWith("N="))
-                {
-                    var mask = term.Substring(2);
-                    rooms = rooms.Where(r => Tools.MatchesMask(r.Name, mask)).ToList();
-                }
-                else if (term.StartsWith("T="))
-                {
-                    var mask = term.Substring(2);
-                    rooms = rooms.Where(r => Tools.MatchesMask(r.Topic, mask)).ToList();
-                }
-                else if (term.StartsWith("Q=") && int.TryParse(term.Substring(2), out var queryLimit))
-                {
-                    rooms = rooms.Take(queryLimit).ToList();
-                }
-                else
-                {
-                    // If it is a category
-                    var category = Resources.ChannelCategoryNames.FirstOrDefault(c => c.Key == term);
-                    if (category.Key != null)
-                    {
-                        // Process category
-                        rooms = rooms.Where(r => r.Category == category.Key).ToList();
-                    }
-                }
-            }
+            if (!FilterByCategory(chatFrame, server, user, ref rooms)) return;
+        }
+
+        if (parameters.Count > 1)
+        {
+            if (!FilterByQueryList(chatFrame, server, user, ref rooms)) return;
         }
 
         // For some reason, don't ask me why, they implemented LISTX as LIST on the Directory Server
@@ -77,5 +50,65 @@ internal class Listx : Command, ICommand
         }
 
         user.Send($":{server} 323 {user} :End of /LIST");
+    }
+
+    private static bool FilterByQueryList(IChatFrame chatFrame, DirectoryServer server, IUser user, ref List<AcsRoomInfo> rooms)
+    {
+        for (var i = 1; i < chatFrame.ChatMessage.Parameters.Count; i++)
+        {
+            var term =  chatFrame.ChatMessage.Parameters[i];
+            if (term == "R=0")
+            {
+                rooms = rooms.Where(r => !r.Managed).ToList();
+            }
+            else if (term == "R=1")
+            {
+                rooms = rooms.Where(r => r.Managed).ToList();
+            }
+            else if (term.StartsWith("N="))
+            {
+                var mask = term.Substring(2).ToUpper();
+                rooms = rooms.Where(r => r.Name.ToUpper().Contains(mask)).ToList();
+            }
+            else if (term.StartsWith("T="))
+            {
+                var mask = term.Substring(2).ToUpper();
+                rooms = rooms.Where(r => r.Topic.ToUpper().Contains(mask)).ToList();
+            }
+            else if (term.StartsWith("Q=") && uint.TryParse(term.Substring(2), out var queryLimit))
+            {
+                if (queryLimit == 0)
+                {
+                    chatFrame.User.Send(Raws.IRCX_ERR_BADCOMMAND_900(server, user, "LISTX"));
+                    return false;
+                }
+                rooms = rooms.Take((int)queryLimit).ToList();
+            }
+            else
+            {
+                // Invalid query term, return error
+                chatFrame.User.Send(Raws.IRCX_ERR_BADCOMMAND_900(server, user, "LISTX"));
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool FilterByCategory(IChatFrame chatFrame, DirectoryServer server, IUser user, ref List<AcsRoomInfo> rooms)
+    {
+        var catcode = chatFrame.ChatMessage.Parameters[0].ToUpper();
+        // If it is a category
+        var category = Resources.ChannelCategoryNames.FirstOrDefault(c => c.Key.ToUpper() == catcode);
+        if (category.Key == null)
+        {
+            // No such category
+            chatFrame.User.Send(Raws.IRCX_RPL_FINDS_NOSUCHCAT_701(server, user));
+            return false;
+        }
+            
+        // Process category
+        rooms = rooms.Where(r => r.Category == category.Key).ToList();
+        return true;
     }
 }
